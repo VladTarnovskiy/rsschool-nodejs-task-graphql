e4d27ac44c9ab7a9c0a0dd67ced793180e27f022
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-07-24 22:10:48 +0300

    feat: add interfaces

---
+++ src/routes/graphql/types/interfaces.ts
@@ -0,0 +1,36 @@
+export interface InputCreatePost {
+  title: string;
+  content: string;
+  authorId: string;
+}
+
+export interface InputCreateUser {
+  name: string;
+  balance: number;
+}
+
+export interface InputCreateProfile {
+  userId: string;
+  isMale: boolean;
+  yearOfBirth: number;
+  memberTypeId: string;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  balance: number;
+}
+
+export interface GraphQLContext {
+  fastify: FastifyInstance;
+  dataLoaders: {
+    profileLoader: DataLoader<string, unknown>;
+    userPostsLoader: DataLoader<string, unknown>;
+    userRelationsLoader: DataLoader<
+      string,
+      { subscribedTo: User[]; subscribedBy: User[] }
+    >;
+    memberTypeLoader: DataLoader<string, unknown>;
+  };
+}
