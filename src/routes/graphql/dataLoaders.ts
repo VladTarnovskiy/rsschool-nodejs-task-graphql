f3a778fe208f61257e72b16afb53d1f2211ac6d3
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-07-25 00:32:14 +0300

    feat: add data loader

---
+++ src/routes/graphql/dataLoaders.ts
@@ -0,0 +1,54 @@
+import { FastifyInstance } from 'fastify';
+
+export const batchProfiles = async (userIds: string[], fastify: FastifyInstance) => {
+  const profiles = await fastify.prisma.profile.findMany({
+    where: { userId: { in: userIds } },
+    include: { memberType: true },
+  });
+  const profileMap = new Map(profiles.map((profile) => [profile.userId, profile]));
+  return userIds.map((userId) => profileMap.get(userId));
+};
+
+export const batchMemberTypes = async (
+  memberTypeIds: string[],
+  fastify: FastifyInstance,
+) => {
+  const memberTypes = await fastify.prisma.memberType.findMany({
+    where: { id: { in: memberTypeIds } },
+  });
+  const memberTypeMap = new Map(
+    memberTypes.map((memberType) => [memberType.id, memberType]),
+  );
+  return memberTypeIds.map((memberTypeId) => memberTypeMap.get(memberTypeId));
+};
+
+export const batchUserPosts = async (userIds: string[], fastify: FastifyInstance) => {
+  const posts = await fastify.prisma.post.findMany({
+    where: { authorId: { in: userIds } },
+  });
+  const postsByAuthor = userIds.map((userId) =>
+    posts.filter((post) => post.authorId === userId),
+  );
+  return postsByAuthor;
+};
+
+export const batchUserRelations = async (userIds: string[], fastify: FastifyInstance) => {
+  const subscriptionsOnAuthor = await fastify.prisma.subscribersOnAuthors.findMany({
+    where: { subscriberId: { in: userIds } },
+    include: { author: true },
+  });
+
+  const subscribers = await fastify.prisma.subscribersOnAuthors.findMany({
+    where: { authorId: { in: userIds } },
+    include: { subscriber: true },
+  });
+
+  return userIds.map((userId) => ({
+    subscribedTo: subscriptionsOnAuthor
+      .filter((sub) => sub.subscriberId === userId)
+      .map((sub) => sub.author),
+    subscribedBy: subscribers
+      .filter((sub) => sub.authorId === userId)
+      .map((sub) => sub.subscriber),
+  }));
+};
--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,15 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
+import depthLimit from 'graphql-depth-limit';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { GraphQLError, graphql, parse, validate } from 'graphql';
+import { graphqlSchema } from './schemas.js';
+import DataLoader from 'dataloader';
+import {
+  batchMemberTypes,
+  batchProfiles,
+  batchUserPosts,
+  batchUserRelations,
+} from './dataLoaders.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -12,8 +21,45 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
-      return {};
+    async handler({ body }) {
+      const { variables, query } = body;
+
+      try {
+        const documentAST = parse(query);
+        const validationErrors = validate(graphqlSchema, documentAST, [depthLimit(5)]);
+
+        if (validationErrors.length > 0) {
+          return { errors: validationErrors };
+        }
+
+        const dataLoaders = {
+          profileLoader: new DataLoader((userIds) =>
+            batchProfiles(userIds as string[], fastify),
+          ),
+          userPostsLoader: new DataLoader((userIds) =>
+            batchUserPosts(userIds as string[], fastify),
+          ),
+          userRelationsLoader: new DataLoader((userIds) =>
+            batchUserRelations(userIds as string[], fastify),
+          ),
+          memberTypeLoader: new DataLoader((memberTypeIds) =>
+            batchMemberTypes(memberTypeIds as string[], fastify),
+          ),
+        };
+
+        const result = await graphql({
+          schema: graphqlSchema,
+          source: query,
+          variableValues: variables,
+          contextValue: { fastify, dataLoaders },
+        });
+
+        return result;
+      } catch (error) {
+        if (error instanceof GraphQLError || error instanceof Error) {
+          return { errors: [{ message: error.message }] };
+        }
+      }
     },
   });
 };
