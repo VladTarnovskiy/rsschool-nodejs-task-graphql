478e1835325b524d13b40d900cab66d0a3a77a94
VladTarnovskiy <vladtarnovski989@gmail.com>
2025-11-22 21:28:21 +0300

    feat: add member type

---
+++ src/routes/graphql/types/member.ts
@@ -0,0 +1,30 @@
+import {
+  GraphQLEnumType,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLObjectType,
+} from 'graphql';
+
+export const MemberTypeIdEnum = new GraphQLEnumType({
+  name: 'MemberTypeId',
+  values: {
+    BASIC: { value: 'BASIC' },
+    BUSINESS: { value: 'BUSINESS' },
+  },
+});
+
+export const MemberType = new GraphQLObjectType({
+  name: 'MemberType',
+  fields: {
+    id: {
+      type: new GraphQLNonNull(MemberTypeIdEnum),
+    },
+    discount: {
+      type: new GraphQLNonNull(GraphQLFloat),
+    },
+    postsLimitPerMonth: {
+      type: new GraphQLNonNull(GraphQLInt),
+    },
+  },
+});
